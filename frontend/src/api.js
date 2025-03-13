b402b9a3c504fd067ebd87d79443ebde4dc6a432
Yuval Nissim <yuvalnissim4@gmail.com>
2025-03-13 12:33:44 +0200

    fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001";
+const API_URL = "mongodb+srv://yuvalnissim:<db_password>@cluster0.sygao8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // ✅ רישום משתמש (כולל isAdmin)
 export async function registerUser(userData) {
